f4db50069186cb092b0005a2ccff276001c0f30e
darkskeeper <dartskeeper@mail.ru>
2016-03-14 11:23:31 +0300

    NO STD::Vector but some fixes

--- main.cpp
+++ main.cpp
@@ -13,21 +13,33 @@ int main(int argc, char *argv[])
         cin >> N;
         int *a = new int[N];
         for (int i = 0; i < N; i++)
+        {
             cin >> a[i];
+        }
         for (int i = 0; i < N; i++)
+        {
             for (int k = i + 1; k < N; k++)
+            {
                 if ( a[k] < a[i] )
                 {
                     int temp = a[k];
                     a[k] = a[i];
                     a[i] = temp;
                 }
+            }
+        }
         int Nkomb = 1;
         for (int n = N - 1; n >= 0 ; n--)
         {
+            if( a[n] >= n )
+            {
+                Nkomb = 0;
+                break;
+            }
             Nkomb = Nkomb * (n - a[n] + 1);
         }
         cout << Nkomb << endl;
+        delete []a;
     }
     return a1.exec();
 }
